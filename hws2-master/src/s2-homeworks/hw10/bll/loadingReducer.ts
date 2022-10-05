f282003ce916bc3522d3f5fa0b03d870516e3362
KirillDanProg <kirill2898v@gmail.com>
2022-10-05 11:31:12 +0300

    hw11 completed SuperRange

--- hws2-master/src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ hws2-master/src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,13 @@
 const initState = {
     isLoading: false,
 }
+type initialStateType = typeof initState
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): initialStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
--- hws2-master/src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ hws2-master/src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
+    const ChangeValueHandler = (event: Event, value: number | number[],  activeThumb: number,) => {
+      props.onChange && props.onChange(event, value, activeThumb)
+    }
+
     return (
         <Slider
+            onChange={ChangeValueHandler}
+
             sx={{ // стили для слайдера // пишет студент
-                
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
