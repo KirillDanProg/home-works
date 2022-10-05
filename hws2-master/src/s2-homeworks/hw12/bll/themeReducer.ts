3b75ec56f2ccf4de9473b172e7e8e018fcfe5db4
KirillDanProg <kirill2898v@gmail.com>
2022-10-05 16:58:24 +0300

    hw12 completed theme mode change

--- hws2-master/src/s2-homeworks/hw12/bll/themeReducer.ts
+++ hws2-master/src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,10 +1,10 @@
 const initState = {
     themeId: 1,
 }
-type InitiStateType = typeof initState
+type InitStateType = typeof initState
 type ActionType = ChangeThemeIdType
 
-export const themeReducer = (state: InitiStateType = initState, action: ActionType): InitiStateType => { // fix any
+export const themeReducer = (state: InitStateType = initState, action: ActionType): InitStateType => { // fix any
     switch (action.type) {
         // дописать
         case 'SET_THEME_ID':
@@ -23,4 +23,4 @@ type ChangeThemeIdType = {
     type:  'SET_THEME_ID',
     id: number
 }
-export const changeThemeId = (id: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', id } as const) // fix any
